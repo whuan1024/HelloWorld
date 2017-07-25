82efcb5b07b4950621c30e67b183edc7f2046df6
wanghuan <whuan1024@gmail.com>
2017-07-25 15:42:34 +0800

    Add a button

--- app/src/main/java/com/example/wanghuan/helloworld/MainActivity.java
+++ app/src/main/java/com/example/wanghuan/helloworld/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.wanghuan.helloworld;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "click button", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
